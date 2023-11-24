e05afa3dd33a78aca10864ce3ff63bed7af439e6
Understudystudent <carlmjames2@gmail.com>
2023-11-24 12:47:01 +0200

    added the comments

--- script/code.js
+++ script/code.js
@@ -1,7 +1,13 @@
-document.getElementById('convertButton').addEventListener('click', CMTS);
 
+/* First we need to get the button and what happens when we click the button*/
+document.querySelectorAll('[data-id]').addEventListener('click', CMTS);
+
+/*CMTS is convert Mintues to Seconds */
 function CMTS() {
+    // we declare minutes and store the value
     let minInput = document.querySelector('[data-mintues]').value;
+    // we need to convert min into seconds
     let secndOutput = minInput * 60;
+    /* we need to display the output */
     document.querySelector('[data-seconds]').value = secndOutput;
 }
